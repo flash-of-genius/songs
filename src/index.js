76a75351dbc49a98079bba7da0f14bcaef99c93a
flash-of-genius <amhdaar@gmail.com>
2022-08-21 07:24:10 +0100

    8 - Wiring Up the Provider

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStoreHook } from "react-redux";
+import App from "./components/App";
+import reducers from "./reducers";
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(
+  <Provider store={createStoreHook(reducers)}>
+    <App />
+  </Provider>,
+
+  document.querySelector("#root"),
+);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,5 @@
+import { combineReducers } from "redux";
+
 const songsReducer = () => {
   return [
     { title: "القلب يعشق كل جميل", duration: "90" },
@@ -14,3 +16,8 @@ const selectedSongReducer = (selectedSong = null, action) => {
 
   return selectedSong;
 };
+
+export default combineReducers({
+  songs: songsReducer,
+  selectedSong: selectedSongReducer,
+});
