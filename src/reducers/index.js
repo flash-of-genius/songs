280669df19c2223e6ac340e9238665572c507395
flash-of-genius <amhdaar@gmail.com>
2022-08-21 07:14:07 +0100

    7 - Building Reducers

---
+++ src/reducers/index.js
@@ -0,0 +1,16 @@
+const songsReducer = () => {
+  return [
+    { title: "القلب يعشق كل جميل", duration: "90" },
+    { title: "حلم", duration: "70" },
+    { title: "شمس الاصيل", duration: "100" },
+    { title: "ثورة الشك", duration: "75" },
+  ];
+};
+
+const selectedSongReducer = (selectedSong = null, action) => {
+  if (action.type === "SONG_SELECTED") {
+    return action.payload;
+  }
+
+  return selectedSong;
+};
